91259397bd01719406ef7995edd4b84df2672a20
Jacob Stuart <stuart4@purdue.edu>
2015-07-03 13:23:56 -0700

    adds navigation drawer

--- app/src/main/java/org/stuartresearch/snapzuisfunner/MainActivity.java
+++ app/src/main/java/org/stuartresearch/snapzuisfunner/MainActivity.java
@@ -1,9 +1,11 @@
 package org.stuartresearch.snapzuisfunner;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new DrawerBuilder().withActivity(this).withTranslucentStatusBar(false).build();
+
     }
 
     @Override
