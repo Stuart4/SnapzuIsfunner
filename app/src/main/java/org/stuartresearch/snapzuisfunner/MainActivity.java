54b24b64052ccc7b5de2a82eba10dbf3dba6d8be
Jacob Stuart <stuart4@purdue.edu>
2015-07-04 09:33:14 -0700

    back button closes drawer

--- app/src/main/java/org/stuartresearch/snapzuisfunner/MainActivity.java
+++ app/src/main/java/org/stuartresearch/snapzuisfunner/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.etsy.android.grid.StaggeredGridView;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,6 +24,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.grid_view) StaggeredGridView gridView;
+
 
     Drawer drawer;
 
@@ -30,14 +38,30 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("SubZeroJake").withEmail("jake@spacejake.com").withIcon(getResources().getDrawable(R.drawable.profile))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+
         drawer = new DrawerBuilder().withActivity(this).withTranslucentStatusBar(false)
                 .withActionBarDrawerToggle(true)
                 .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
                 .addDrawerItems(
                         new PrimaryDrawerItem().withName("Profile").withIcon(R.drawable.ic_account_box_black_18dp),
                         new PrimaryDrawerItem().withName("Messages").withIcon(R.drawable.ic_message_black_18dp),
-                        new PrimaryDrawerItem().withName("User").withIcon(R.drawable.ic_group_black_18dp),
-                        new PrimaryDrawerItem().withName("Tribe").withIcon(R.drawable.ic_filter_tilt_shift_black_18dp),
+                        new PrimaryDrawerItem().withName("Open User").withIcon(R.drawable.ic_group_black_18dp),
+                        new PrimaryDrawerItem().withName("Open Tribe").withIcon(R.drawable.ic_filter_tilt_shift_black_18dp),
                         new DividerDrawerItem()
                 )
                 .addStickyDrawerItems(
@@ -70,5 +94,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer != null && drawer.isDrawerOpen()) {
+            drawer.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
